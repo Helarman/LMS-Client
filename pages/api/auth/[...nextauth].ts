1fe3ad407453a7c6e9df1cc0eae406291198c1f3
Helarman <helarman@bk.ru>
2024-02-13 21:27:42 +0300

    fix auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,11 +14,20 @@ export default NextAuth({
       session.jwt = token.jwt;
       return Promise.resolve(session);
     },
-    jwt: async ({ token, user }: { token: any, user: any }) => {
+    jwt: async ({ token, user, account }: { token: any, user: any, account: any }) => {
       const isSignIn = user ? true : false;
-      if (isSignIn) {
-        token.id = user.id;
-        token.jwt = user.jwt;
+      if (isSignIn && account) {
+        try {
+          const public_url = process.env.NEXT_PUBLIC_API_URL;
+          const response = await fetch(
+            `${public_url}/api/auth/${account.provider}/callback?access_token=${account?.access_token}`
+          );
+          const data = await response.json();
+          token.jwt = data.jwt;
+          token.id = data.user.id;
+        } catch (error) {
+          console.error('Fetch failed:', error);
+        }
       }
       return Promise.resolve(token);
     },
